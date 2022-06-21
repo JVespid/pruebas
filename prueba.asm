872fb9188eabc5115f8af410292326bfa07956e0
JVespid <bernardo.abel.ls1@gmail.com>
2022-06-21 07:51:07 -0500

    agregando plantillas

---
+++ prueba.asm
@@ -0,0 +1,30 @@
+
+org 100h
+.model smail
+
+.data 
+.code
+
+;la instruccion add suma 2 valores y guarda el resultado en el primer valor dado, 
+;add al, bl
+
+;la instruccion sub resta 2 valores, y guarda el valor en el primero al = al - bl 
+;sub al, bl
+
+;la instruccion mul multiplica por "al" lo que le allamos enviado
+;mul bl
+
+;la instruccion div, divide un valor con al y el resultado lo guarda en registro al
+;div bl
+
+;la instruccion aam separa las decenas de las unidades que estan en al poniendo en el registro ah las decenas y en el al las unidades
+;aam
+
+;la instruccion aas resta 30h al registro al, para obtener numeros facilmente
+;aas
+
+
+
+
+
+.exit
